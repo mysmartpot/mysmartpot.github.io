c24c12ded02ba48b1b25a1e783062a18ddf8fd36
Justin Andresen <jan.dresen.95@gmail.com>
2022-03-26 15:44:55 +0100

    Add skeleton for website

---
+++ main.js
@@ -0,0 +1,23 @@
+(function() {
+  // Modify links to anchors such that elements are smoothly scrolled into view.
+  const internalLinks = document.querySelectorAll('a[href^="#"]');
+  for (const link of internalLinks) {
+    link.addEventListener('click', (event) => {
+      const targetName = link.getAttribute('href').substring(1);
+      const targets = targetName.length === 0
+        ? [document.body]
+        : document.getElementsByName(targetName);
+      if (targets.length > 0) {
+        targets[0].scrollIntoView({ behavior: 'smooth' });
+        window.history.pushState({}, '', link.href);
+        event.preventDefault();
+      }
+    });
+  }
+
+  // Modify links to external sites such that they are opened in a new tab.
+  const externalLinks = document.querySelectorAll('a[href^="https://"]');
+  for (const link of externalLinks) {
+    link.target = '_blank';
+  }
+})();
